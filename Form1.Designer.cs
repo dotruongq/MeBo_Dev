2d3c64c0ca3f3e67d35d06148c5f0d577668b2b4
Ken <dotruongq@gmail.com>
2018-06-04 19:55:16 -0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace MeBo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.selllabel = new System.Windows.Forms.Label();
+            this.buylabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.timercandle = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.text2 = new System.Windows.Forms.Label();
+            this.text1 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selllabel
+            // 
+            this.selllabel.AutoEllipsis = true;
+            this.selllabel.AutoSize = true;
+            this.selllabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.selllabel.Location = new System.Drawing.Point(12, 9);
+            this.selllabel.Name = "selllabel";
+            this.selllabel.Size = new System.Drawing.Size(35, 13);
+            this.selllabel.TabIndex = 2;
+            this.selllabel.Text = "label1";
+            this.selllabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buylabel
+            // 
+            this.buylabel.AutoSize = true;
+            this.buylabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.buylabel.Location = new System.Drawing.Point(12, 37);
+            this.buylabel.Name = "buylabel";
+            this.buylabel.Size = new System.Drawing.Size(35, 13);
+            this.buylabel.TabIndex = 3;
+            this.buylabel.Text = "label2";
+            this.buylabel.Click += new System.EventHandler(this.buylabel_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(123, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "LONG";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(249, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(123, -1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "SHORT";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(387, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "FORMATION";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(259, 36);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Cancel ALL ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(162, 340);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 13;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1m",
+            "3m ",
+            "1h",
+            "1d"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 340);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 367);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1131, 168);
+            this.dataGridView1.TabIndex = 16;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(310, 344);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(88, 17);
+            this.checkBox2.TabIndex = 17;
+            this.checkBox2.Text = "Auto Refresh";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // timercandle
+            // 
+            this.timercandle.Interval = 3000;
+            this.timercandle.Tick += new System.EventHandler(this.timercandle_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(561, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // text2
+            // 
+            this.text2.AutoSize = true;
+            this.text2.Location = new System.Drawing.Point(65, 9);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(30, 13);
+            this.text2.TabIndex = 5;
+            this.text2.Text = "text2";
+            this.text2.Click += new System.EventHandler(this.text2_Click);
+            // 
+            // text1
+            // 
+            this.text1.AutoSize = true;
+            this.text1.Location = new System.Drawing.Point(65, 37);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(30, 13);
+            this.text1.TabIndex = 4;
+            this.text1.Text = "text1";
+            this.text1.Click += new System.EventHandler(this.text1_Click);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(416, 340);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(112, 20);
+            this.numericUpDown3.TabIndex = 19;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(572, 340);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown4.TabIndex = 20;
+            this.numericUpDown4.ValueChanged += new System.EventHandler(this.numericUpDown4_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(537, 344);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "MA1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(704, 345);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "MA2";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(707, 27);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(155, 69);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "START";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(883, 27);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(43, 17);
+            this.radioButton1.TabIndex = 23;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Buy";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(883, 53);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(42, 17);
+            this.radioButton2.TabIndex = 24;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Sell";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(883, 79);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(57, 17);
+            this.radioButton3.TabIndex = 25;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Switch";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Trading Indicators";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(958, 75);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 28;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(883, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1484, 661);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.text1);
+            this.Controls.Add(this.buylabel);
+            this.Controls.Add(this.selllabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label selllabel;
+        private System.Windows.Forms.Label buylabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Timer timercandle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label text2;
+        private System.Windows.Forms.Label text1;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,309 @@
+﻿using BitMEX;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+
+namespace MeBo
+{
+    public partial class Form1 : Form
+    {
+        public string Symbol = "XBTUSD";
+        double Price = 0;
+        double realliquid = 0;
+
+        BitMEXApi bitmex = new BitMEXApi();
+        List<OrderBook> CurrentBook = new List<OrderBook>();
+        List<candle> CanDles = new List<candle>();
+
+        bool Running = false;
+        string Mode = "Wait";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitDropDown();
+            timercandle.Start();
+            // string result = bitmex.GetOrders(Symbol);   // OPEN ORDER CHECK JSON
+            //  MessageBox.Show(result);
+
+            //MessageBox.Show(CurrentOrderPrice(Symbol, "Buy").ToString());
+            // while(true)
+            // {
+            text1.Text = "Buy";
+            text2.Text = "Sell";
+
+
+
+        }
+
+        private void InitDropDown()
+        {
+            comboBox1.SelectedIndex = 0; // no choosing combo box.
+                                         //  comboBox2.SelectedIndex = 0;
+        }
+        private double CurrentOrderPrice(string Symbol, string Side)
+        {
+            CurrentBook = bitmex.GetOrderBook(Symbol, 3); // look for number 1 depth
+            double SellPrice = CurrentBook.Where(a => a.Side == "Sell").FirstOrDefault().Price;
+            double BuyPrice = CurrentBook.Where(a => a.Side == "Buy").FirstOrDefault().Price;
+            double sell1 = CurrentBook.Where(a => a.Side == "Sell").ElementAt(2).Price;
+           
+
+            double OrderPrice = 0;
+            switch (Side)
+            {
+                case "Buy":
+                    OrderPrice = BuyPrice;
+
+                    break;
+
+                case "Sell":
+                    OrderPrice = sell1;
+                    break;
+                    //case "Sell":
+                    //   OrderPrice = sell1;
+                    //break;
+
+
+
+
+
+            }
+            return OrderPrice;
+
+        }
+        private void PnlUpdate()
+        {
+            label6.Text = bitmex.GetPNL(Symbol,realliquid);
+
+        }
+
+
+        private void PriceUpdate()
+        {
+            buylabel.Text = (CurrentOrderPrice(Symbol, "Buy").ToString());
+            selllabel.Text = (CurrentOrderPrice(Symbol, "Sell").ToString());
+            label3.Text = (CurrentOrderPrice(Symbol, "Sell_1").ToString());
+        }
+        private void MakeOrder(string Symbol, string Side, int Qty, double Price = 0) // Price ==0 to be reference, to overide later 
+        {
+            switch (Side)
+            {
+                case "Buy":
+                    if(Price==0)
+                    {
+                        Price = CurrentOrderPrice(Symbol, Side);
+                    }
+                    var MarketBuy = bitmex.PostOrderPostOnly(Symbol, Side, Price, Qty);
+
+                    break;
+
+                case "Sell":
+                    if(Price==0)
+                    {
+                        Price = CurrentOrderPrice(Symbol, Side); 
+                    }
+                    var MarketSell = bitmex.PostOrderPostOnly(Symbol, Side, Price, Qty);
+                    break;
+            }
+        }
+
+        private void MakeFormation(string Symbol, string Side, int Qty, double Price = 0) // Price ==0 to be reference, to overide later 
+        {
+            //switch (Side)
+            // {
+            //  case "Buy":
+            
+            for (int i = 1; i <= 5; i++)
+            {
+                if (Price == 0)
+                {
+                    Price = CurrentOrderPrice(Symbol, Side);
+                }
+                else
+                {
+                    Price = CurrentOrderPrice(Symbol, Side) + (i*10);
+                }
+                var MarketBuy = bitmex.PostOrderPostOnly(Symbol, "Sell", Price, Qty);
+            }
+            //   break;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                if (Price == 0)
+                {
+                    Price = CurrentOrderPrice(Symbol, Side);
+                }
+                else
+                {
+                    Price = CurrentOrderPrice(Symbol, Side) - (i * 10);
+                }
+                var MarketBuy = bitmex.PostOrderPostOnly(Symbol, "Buy", Price, Qty);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void buylabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void text2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MakeOrder(Symbol, "Buy", Convert.ToInt32(numericUpDown1.Value));
+        }
+
+     
+
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MakeOrder(Symbol, "Sell", Convert.ToInt32(numericUpDown1.Value));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                bitmex.CancelAllOpenOrders(Symbol);
+            }
+            else
+            MakeFormation(Symbol, "Buy", Convert.ToInt32(numericUpDown1.Value));
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+     /*  private void Updatecandle()
+        {
+            CanDles = bitmex.GetCandle(Symbol, Convert.ToInt32(numericUpDown2.Value), comboBox1.SelectedItem.ToString());
+
+            foreach(candle c in CanDles)
+            {
+                c.PCC = CanDles.Where(a => a.TimeStamp < c.TimeStamp).Count();
+
+                int MA1Period = Convert.ToInt32(numericUpDown3.Value);
+                int MA2Period = Convert.ToInt32(numericUpDown4.Value);
+
+                if(c.PCC > MA1Period)
+                {
+                    c.MA1 = CanDles.Where(a => a.TimeStamp <= c.TimeStamp).OrderByDescending(a => a.TimeStamp).Take(MA1Period).Average(a => a.Close);
+
+                }
+                if(c.PCC > MA2Period)
+                {
+                    c.MA2 = CanDles.Where(a => a.TimeStamp <= c.TimeStamp).OrderByDescending(a => a.TimeStamp).Take(MA2Period).Average(a => a.Close);
+
+                }
+
+
+
+            }
+
+
+
+            dataGridView1.DataSource = CanDles;
+        }*/
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timercandle_Tick(object sender, EventArgs e)
+        {
+            if(checkBox2.Checked)
+            {
+               // Updatecandle();
+                PriceUpdate();
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void text1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) //START
+        {
+           
+              //  Updatecandle();
+                PriceUpdate();
+                PnlUpdate();
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
